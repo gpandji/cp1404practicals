ea1c0b42b96894d4f44ae4bf87f2dba4f34c836f
My Name <myemail@email.com>
2024-07-20 18:48:23 +0800

    Completed prac 7 Completed More Guitars exercise

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -10,13 +10,36 @@ def load_guitars(filename):
 
     for row in reader:
         guitars.append(Guitar(row[0], int(row[1]), float(row[2])))
+    in_file.close()
     return guitars
+
+
+def add_new_guitars(guitars):
+    """Add new guitars to the list."""
+    print("Enter your new guitars:")
+    name = input("Name: ")
+    while name:
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitars.append(Guitar(name, year, cost))
+        name = input("Name: ")
+
+
+def save_guitars(filename, guitars):
+    """Save the list of guitars to a CSV file."""
+    with open(filename, 'w', newline='') as file:
+        writer = csv.writer(file)
+        for guitar in guitars:
+            writer.writerow([guitar.name, guitar.year, guitar.cost])
 
 
 def main():
     guitars = load_guitars('guitars.csv')
+    add_new_guitars(guitars)
+    guitars.sort()
     for guitar in guitars:
         print(guitar)
+    save_guitars('guitars.csv', guitars)
 
 
 main()
